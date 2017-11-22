1992e7289b2e1b5a91c560be49341323e8d817b7
Fernando <fernando_carmo500@hotmail.com>
2017-11-21 22:06:40 -0200

    something left behind

--- RHControl/CargoControl.cs
+++ RHControl/CargoControl.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RHControl
 {
-    public class Class1
+    public class CargoControl
     {
     }
 }
--- RHControl/SetorControl.cs
+++ RHControl/SetorControl.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RHControl
 {
-    class Class1
+    class SetorControl
     {
     }
 }
